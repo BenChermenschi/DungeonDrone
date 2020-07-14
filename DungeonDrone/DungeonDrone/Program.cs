5643720805d15d78b071973624931e3accea4ec4
Unknown <chermenschi.ben@gmail.com>
2020-07-14 13:08:55 +0200

    Creating project and importing discord.net framework

---
+++ DungeonDrone/DungeonDrone/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace DungeonDrone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
